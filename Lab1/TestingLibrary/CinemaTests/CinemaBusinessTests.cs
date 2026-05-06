ab9cc27779cbf6a2629d9703e22e478c31d4f0de
Flexlikeuuu <kirillstepanenko2017@mail.ru>
2026-05-06 15:16:22 +0300

    Change shared context

--- Lab1/TestingLibrary/CinemaTests/CinemaBusinessTests.cs
+++ Lab1/TestingLibrary/CinemaTests/CinemaBusinessTests.cs
@@ -1,7 +1,7 @@
-﻿using TestingLibrary;
-using CinemaLogic;
+﻿using CinemaLogic;
 using System.Threading.Tasks;
 using System;
+using TestingLibrary;
 
 namespace CinemaTests
 {
@@ -20,17 +20,42 @@ namespace CinemaTests
             _testUser = new User { Name = "Alice", Age = 25, Balance = 50, Region = "EU", ConnectedDevices = 1 };
         }
 
+        [TestMethod("Запись данных в Shared Context")]
+        public void Step1_WriteToSharedContext()
+        {
+            _context.Set("GlobalPromo", "SAVE50");
+            _context.Set("LastUserRegion", _testUser.Region);
+
+            Assert.IsNotNull(_context.Get<string>("GlobalPromo"));
+        }
+
+        [TestMethod("Чтение данных из Shared Context")]
+        public void Step2_ReadFromSharedContext()
+        {
+            string promo = _context.Get<string>("GlobalPromo");
+            string region = _context.Get<string>("LastUserRegion");
+
+            Assert.AreEqual("SAVE50", promo);
+            Assert.AreEqual("EU", region);
+
+            decimal price = _billing.CalculatePrice(SubscriptionType.Standard, region);
+            decimal discounted = _billing.ApplyPromoCode(promo, price);
+
+            Assert.AreEqual(5.5m, discounted);
+        }
+
         [TestMethod("Цена в US")]
         public void TestRegionalPricing()
         {
             decimal price = _billing.CalculatePrice(SubscriptionType.Premium, "US");
-            Assert.AreEqual(24m, price);
+            Assert.AreEqual(25m, price);
             Assert.IsInRange((double)price, 20, 30);
         }
+
         [TestCase(SubscriptionType.Premium, "EU", 22.0)]
         [TestCase(SubscriptionType.Standard, "RU", 10.0)]
+        [TestCase(SubscriptionType.Standard, "US", 5.0)]
         [TestCase(SubscriptionType.Free, "RU", 0.0)]
-        [TestCase(SubscriptionType.Standard, "US", 999.0)] 
         public void TestPriceCases(SubscriptionType type, string reg, double expected)
         {
             decimal price = _billing.CalculatePrice(type, reg);
@@ -45,21 +70,6 @@ namespace CinemaTests
 
             _testUser.Age = 10;
             Assert.IsFalse(_billing.CanWatch(_testUser, movie));
-        }
-        [TestMethod("Тест лимита устройств (Провальный)")]
-        public void TestDeviceLimitFail()
-        {
-            _testUser.ConnectedDevices = 5;
-            var movie = new Movie { Title = "Cartoon", MinAge = 0 };
-            Assert.IsTrue(_billing.CanWatch(_testUser, movie));
-        }
-
-        [TestMethod("Промокоды")]
-        public void TestPromos()
-        {
-            decimal price = _billing.ApplyPromoCode("SAVE50", 100m);
-            Assert.AreEqual(50m, price);
-            Assert.AreNotEqual(100m, price);
         }
 
         [TestMethod("Асинхронная оплата")]
@@ -83,31 +93,7 @@ namespace CinemaTests
         {
             Assert.IsNotNull(_testUser);
             Assert.IsInstanceOf<User>(_testUser);
-            Assert.StringContains("Ali", _testUser.Name);
-
-            object nullObj = null;
-            Assert.IsNull(nullObj);
-        }
-
-        [TestMethod("Тест на содержание строки")]
-        public void TestStringFail()
-        {
             Assert.StringContains("Bob", _testUser.Name);
-        }
-        [TestMethod("Проверка работы Shared Context")]
-        public void TestSharedContextUsage()
-        {
-            _context.Set("SavedPromoCode", "FREE");
-            _context.Set("TempDiscount", 50m);
-
-            string code = _context.Get<string>("SavedPromoCode");
-            decimal discount = _context.Get<decimal>("TempDiscount");
-
-            string notFound = _context.Get<string>("NonExistentKey");
-
-            Assert.AreEqual("FREE", code);
-            Assert.AreEqual(50m, discount);
-            Assert.IsNull(notFound);
         }
 
         [After]
